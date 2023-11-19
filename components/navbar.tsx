05c6ecad8759b9b2ea3cab6418750f7f4155adeb
RemiLisciandra <rlisciandra@gmail.com>
2023-11-19 17:08:51 +0100

    Ajout logo entreprise

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Image from 'next/image';
 
 import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
@@ -22,6 +23,7 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
+      <Image src="/logo.png" alt="Logo of company" width={60} height={60} priority/>
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
