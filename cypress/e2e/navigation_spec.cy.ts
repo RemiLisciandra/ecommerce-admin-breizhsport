f472d946f335dd9f8b4dd48dbaf19125a0da1a15
RemiLisciandra <rlisciandra@gmail.com>
2024-03-19 22:09:02 +0100

    Test

--- cypress/e2e/navigation_spec.cy.ts
+++ cypress/e2e/navigation_spec.cy.ts
@@ -1,26 +1,20 @@
 describe("Navigation des catégories", () => {
   beforeEach(() => {
+    cy.visit("https://localhost:3001");
+  });
+
+  it("doit contenir un texte spécifique", () => {
     cy.visit("http://localhost:3001/");
+    cy.contains("Featured Products");
   });
 
-  it("devrait naviguer vers la catégorie Shoes", () => {
-    cy.get('a[href="/category/f66009d6-b912-48ac-8b5d-33b1b3fa9356"]').click();
-    cy.url().should(
-      "include",
-      "/category/f66009d6-b912-48ac-8b5d-33b1b3fa9356"
-    );
+  it("doit contenir un texte spécifique", () => {
+    cy.visit("http://localhost:3001/");
+    cy.contains("Shoes");
   });
 
-  it("devrait naviguer vers la catégorie Jackets", () => {
-    cy.get('a[href="/category/1bb45699-2c27-424d-a7b3-21b3debf126d"]').click();
-    cy.url().should(
-      "include",
-      "/category/1bb45699-2c27-424d-a7b3-21b3debf126d"
-    );
-  });
-
-  it("devrait naviguer vers la page d’accueil via le logo", () => {
-    cy.get('a[href="/"]').click();
-    cy.url().should("eq", "http://localhost:3001/");
+  it("doit contenir un texte spécifique", () => {
+    cy.visit("http://localhost:3001/");
+    cy.contains("Jackets");
   });
 });
